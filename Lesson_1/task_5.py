6e16cdf9a58ac4f93b6432d2dd48991fc0446557
Az OV <olazar61@gmail.com>
2023-01-28 16:49:47 +0300

    Lesson_1.v.2

--- Lesson_1/task_5.py
+++ Lesson_1/task_5.py
@@ -2,9 +2,8 @@
 
 any_number = float(input('Введите число: '))
 sum_of_numbers = 0
-while int(any_number * 10) - any_number * 10 != 0:
+while int(any_number) - any_number != 0:
     any_number = any_number * 10
-any_number = any_number * 10
 while any_number // 10 != 0:
     sum_of_numbers += any_number % 10
     any_number = any_number // 10
